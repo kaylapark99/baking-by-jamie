1782b8901529e8cc31f732bfd161608e97295db8
kaylapark99 <kpark@colgate.edu>
2021-02-23 11:28:25 -0500

    Orgamizing files, commenting

--- Tech/scripts.js
+++ Tech/scripts.js
@@ -1,12 +1,19 @@
-/* */
+/* BAKED GOODS ELEMENTS: INTERACTIVELY SWITCH BETWEEN BAKED GOOD PICTURES */
 const goodies = document.querySelectorAll(".goodies-options li");
 var current_goodie = document.querySelector("div.cakes");
 var current_clicked = document.querySelector("li.cakes");
 var change_goodie = current_goodie.classList;
 var next_goodie = null;
 
+/* NAV ELEMENTS: SMOOTHLY SCROLL TO ELEMENTS ON PAGE */
+const nav = document.querySelectorAll(".nav-options li a");
+const nav_height = document.querySelector(".nav").offsetHeight;
+var current_loc = document.querySelector(".home");
+var next_loc = null;
+
+/* when clicked on a specific baked good li element, switch display 
+pictures and toggle "clicked" for li element */
 function switch_pics() {
-  console.log(this);
   current_clicked.classList.remove("clicked");
   this.classList.add("clicked");
   change_goodie = this.classList;
@@ -17,11 +24,7 @@ function switch_pics() {
   next_goodie.classList.remove("hide");
 }
 
-const nav = document.querySelectorAll(".nav-options li a");
-const nav_height = document.querySelector(".nav").offsetHeight;
-var current_loc = document.querySelector(".home");
-var next_loc = null;
-
+/* when clicked on specific nav link, smoothly scroll to its position on page */
 function pageScroll() {
   current_loc = this.classList;
   next_loc = document.getElementById(current_loc);
